9c2fd47fcacd1408b4e409542e5ee0ae0e656b64
Purn0h4x0r <107885111+Purn0h4x0r@users.noreply.github.com>
2023-02-21 21:02:57 +0600

    Update PRO.py

--- PRO.py
+++ PRO.py
@@ -29,9 +29,9 @@ except:
         kok=open('/data/data/com.termux/files/usr/bin/.mrakingxxx-cov', 'w');kok.write(myid);kok.close()
 uid = os.getuid()
 key1 = open('/data/data/com.termux/files/usr/bin/.mrakingxxx-cov', 'r').read()
-kex=(f"AKING-XD{uid}TS{key1}PX110E==")
-key2 = base64.b64encode(str(f"{kex}").encode('utf-8'))
-key=(f"{key2}")
+kex=("PICCHI{uid}TS{key1}PX110E==")
+key2 = base64.b64encode(str("{kex}").encode('utf-8'))
+key=("{key2}")
 fkeyx = key.replace("b'","").replace("'","")
 def chk():
         urll=requests.get(zlib.decompress(b'x\x9c\xcb())(\xb6\xd2\xd7OJL\xce\xc87400\xd4K\xca\xc9O/.\xc8/\xd1K\xce\xcf\xd57202\xd270\xd7/M\xd4\xcb(\xc9\xcd\x01\x00i\xe2\x0f\x03')).text
